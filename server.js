216b68f192d4902b2ad2cf7d3d3bbdfecf010d1b
Jon Keopangna <jkeopangna@gmail.com>
2021-01-17 17:18:55 -0700

    Passed data to render when viewing all pets, added code to post a pet

--- server.js
+++ server.js
@@ -6,10 +6,12 @@ const PORT = process.env.PORT || 8080;
 
 // Requiring our models for syncing
 const db = require('./models');
+const bodyparser = require('body-parser');
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(bodyparser.json());
 
 // Static directory
 app.use(express.static('public'));
