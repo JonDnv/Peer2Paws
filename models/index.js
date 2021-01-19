46851a5907276d6759632092370f66ec3b641bf6
Jon Godwin <45859095+JonDnv@users.noreply.github.com>
2021-01-19 10:43:42 -0700

    Finished ENV & Working on Dog & Cat APIs

--- models/index.js
+++ models/index.js
@@ -4,7 +4,7 @@ var fs = require("fs");
 var path = require("path");
 var Sequelize = require("sequelize");
 var basename = path.basename(module.filename);
-var env = process.env.NODE_ENV || "development";
+var env = process.env.NODE_ENV || "production";
 var config = require(__dirname + "/../config/config.json")[env];
 var db = {};
 
@@ -12,10 +12,17 @@ if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
   var sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
+    process.env.DBNAME,
+    process.env.DBUSER,
+    process.env.DBPASSWORD,
+    {
+      host: process.env.DBHOST,
+      dialect: config.dialect,
+    }
+    // config.database,
+    // config.username,
+    // config.password,
+    // config
   );
 }
 
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,38 +1,42 @@
+module.exports = function (app) {
+  //Routes for different pages - NAV BAR
+  app.get("/", (req, res) => {
+    res.render("index");
+  });
 
-module.exports = function(app) {
-    //Routes for different pages - NAV BAR
-    app.get("/", (req, res) => {
-        res.render("index")
-    });
+  app.get("/sign-in", (req, res) => {
+    res.render("sign-in");
+  });
 
-    app.get("/sign-in", (req, res) => {
-        res.render("sign-in")
-    });
+  app.get("/contact", (req, res) => {
+    res.render("contact");
+  });
 
-    app.get("/contact", (req, res)=> {
-        res.render("contact")
-    });
+  //Routes for different pages - SIDE BAR
 
-    //Routes for different pages - SIDE BAR
-    
-    app.get("/see-all", (req, res)=> {
-        res.render("see-all")
-    });
+  app.get("/see-all", (req, res) => {
+    res.render("see-all");
+  });
 
-    app.get("/specificpets", (req, res) => {
-        res.render("specificpets")
-    });
+  app.get("/specificpets", (req, res) => {
+    res.render("specificpets");
+  });
 
-    app.get("/post-pet", (req,res) => {
-        res.render("post-pet")
-    });
+  app.get("/post-pet", (req, res) => {
+    res.render("post-pet");
+  });
 
-    app.get("/apply", (req,res) => {
-        res.render("apply")
-    });
+  app.get("/apply", (req, res) => {
+    res.render("apply");
+  });
 
-    //View nearby shelters route
-    app.get("/near-me", (req, res) => {
-        res.render("near-me")
-    });
-}
+  //View cat breed route
+  app.get("/cat-breed", (req, res) => {
+    res.render("catbreed");
+  });
+
+  //View dog breed route
+  app.get("/dog-breed", (req, res) => {
+    res.render("dogbreed");
+  });
+};
