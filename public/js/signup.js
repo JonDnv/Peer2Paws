cde11e0e1ddd9d47856ccaf5814d53af7c500126
Jon Keopangna <jkeopangna@gmail.com>
2021-01-20 00:26:10 -0700

    Pushing the sign-up for updates additions

---
+++ public/js/signup.js
@@ -0,0 +1,37 @@
+
+
+const updateBtn = document.getElementById('verifyBtn');
+updateBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+//JK--Retrieve data of New Member--//
+    const newMember = {
+        Name: document.getElementById('Name').value.trim(),
+        Email: document.getElementById('Email').value.trim(),
+    };
+
+//JK--Send the POST request to update the Members list-//
+
+fetch('/api/signup', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newMember),
+})
+    .then((response) => response.json())
+    .then((data) => {
+        console.log('New Member sign-up', data);
+        alert(`You signed-up for updates!`);
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+        alert(error);
+    });
+
+//JK--Clearing the form--//
+
+document.getElementById('Name').value = '';
+document.getElementById('Email').value = '';
+
+});
